b21d318c7b9654f94b29d68c511cf8d5845ef409
SimonbJohnson <simonbjohnson@gmail.com>
2015-04-23 20:41:21 +0100

    Set brush extent presets + simple meta data for download

--- js/foodprices.js
+++ js/foodprices.js
@@ -201,8 +201,9 @@ function generateChartView(cf,adm0,prod,unit,adm0_code){
     cf.byAdm1.filterAll(); 
     cf.byMkt.filterAll();    
     
+    var title = 'Price of ' + prod + ' per ' + unit + ' in '+adm0;
+    var html = '<h4>'+title+'</h4><p>';
     
-    var html = '<h4>Price of ' + prod + ' per ' + unit + ' in '+adm0+'</h4><p>';
     if(embedded ==='true'){
         html += '<a id="maplink" href="">Map</a> > ';
     }
@@ -219,7 +220,7 @@ function generateChartView(cf,adm0,prod,unit,adm0_code){
        backToMap();
     });
     generateBarChart(getAVG(cf.groupByAdm1Sum.all(),cf.groupByAdm1Count.all()),cf,prod,unit,adm0,adm0_code);
-    generateTimeCharts(getAVG(cf.groupByDateSum.all(),cf.groupByDateCount.all()),cf);
+    generateTimeCharts(getAVG(cf.groupByDateSum.all(),cf.groupByDateCount.all()),cf,title);
     
 
 }
@@ -235,7 +236,10 @@ function generateADMChartView(cf,adm1,prod,unit,adm0,adm0_code){
     }
     
     curLevel = 'adm1';
-    var html = '<h4>Price of ' + prod + ' per ' + unit + ' in '+adm1+'</h4><p>';
+    
+    var title = 'Price of ' + prod + ' per ' + unit + ' in '+adm1;    
+    var html = '<h4>'+title+'</h4><p>';
+    
     if(embedded ==='true'){
         html += '<a id="maplink" href="">Map</a> > ';
     }
@@ -260,7 +264,7 @@ function generateADMChartView(cf,adm1,prod,unit,adm0,adm0_code){
     cf.byMkt.filterAll();
     cf.byAdm1.filter(adm1);    
     generateBarChart(getAVG(cf.groupByMktSum.all(),cf.groupByMktCount.all()),cf,prod,unit,adm0,adm0_code,adm1);
-    generateTimeCharts(getAVG(cf.groupByDateSum.all(),cf.groupByDateCount.all()),cf);
+    generateTimeCharts(getAVG(cf.groupByDateSum.all(),cf.groupByDateCount.all()),cf,title);
     
 
 }
@@ -277,10 +281,14 @@ function generateMktChartView(cf,mkt,prod,unit,adm0,adm0_code,adm1){
     
     curLevel = 'mkt';
     
-    var html = '<h4>Price of ' + prod + ' per ' + unit + ' in '+mkt+'</h4><p>';
+    var title = 'Price of ' + prod + ' per ' + unit + ' in '+mkt;
+    
+    var html = '<h4>'+title+'</h4><p>';
+    
     if(embedded ==='true'){
         html += '<a id="maplink" href="">Map</a> > ';
     }
+    
     html +='<a id="adm0link" href="">'+adm0+'</a> > <a id="prodlink" href="">' + prod + '</a> > <a id="adm1link" href="">' + adm1 + '</a> > ' + mkt + '</p>';
     $(targetHeader).html(html);
     $(targetDiv).html('<div class="row"><div id="nav_chart" class="col-xs-12"></div></div><div class="row"><div id="main_chart" class="col-xs-12"></div></div><div class="row"><div id="drilldown_chart" class="col-xs-12"></div></div>');
@@ -306,7 +314,7 @@ function generateMktChartView(cf,mkt,prod,unit,adm0,adm0_code,adm1){
     cf.byDate.filterAll();
     cf.byMkt.filter(mkt);    
     
-    generateTimeCharts(getAVG(cf.groupByDateSum.all(),cf.groupByDateCount.all()),cf);
+    generateTimeCharts(getAVG(cf.groupByDateSum.all(),cf.groupByDateCount.all()),cf,title);
 }
 
 function getAVG(sum,count){
@@ -322,9 +330,19 @@ function getAVG(sum,count){
     return data;    
 }
 
-function generateTimeCharts(data,cf){
+function generateTimeCharts(data,cf,title){
     
-    $('#nav_chart').html('<p>Select a portion of the chart below to zoom in the data.</p>');
+    $('#nav_chart').html('<p>Select a portion of the chart below to zoom in the data.</p><p><span id="brush6" class="setbrush">Last 6 months</span><span id="brush12" class="setbrush">1 year</span><span id="brush60" class="setbrush">5 years</span></p>');
+
+    $('#brush6').click(function(){
+        setBrushExtent(data,6);
+    });
+    $('#brush12').click(function(){
+        setBrushExtent(data,12);
+    });
+    $('#brush60').click(function(){
+        setBrushExtent(data,60);
+    });
 
     var margin = {top: 10, right: 20, bottom: 20, left: 60},
         width = $('#nav_chart').width() - margin.left - margin.right,
@@ -345,7 +363,7 @@ function generateTimeCharts(data,cf){
         ];        
 
     var brush = d3.svg.brush()
-        .x(x2)
+        .x(x2)        
         .on("brush", brushed)
         .on("brushend", function(){
                            
@@ -409,7 +427,7 @@ function generateTimeCharts(data,cf){
             .attr("cy",10)
             .attr("r", 4)
             .attr("fill","#ffffff")
-            .attr("stroke","#5fbbff");
+            .attr("stroke","#6fbfff");
 
         price.append("text")
             .attr("x", 9)
@@ -432,7 +450,11 @@ function generateTimeCharts(data,cf){
                 d = x0 - d0.key > d1.key - x0 ? d1 : d0;
             price.attr("transform", "translate(" + (x(d.key)+margin.left) + "," + (y(d.value)+margin.top) + ")");
             var value = d.value<100 ? d.value.toPrecision(3) : Math.round(d.value);
-            price.select("text").text(value);
+            var m_names = new Array('Jan', 'Feb', 'Mar', 
+                'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 
+                'Oct', 'Nov', 'Dec'); 
+            var date = m_names[d.key.getMonth()] + '-' + d.key.getFullYear();
+            price.select("text").text(date+": "+value);
         });
 
     var linedata = [];
@@ -451,7 +473,7 @@ function generateTimeCharts(data,cf){
         .enter().append("path")
         .attr("class", "priceline")
         .attr("d", line)
-        .attr("stroke","#5fbbff")
+        .attr("stroke","#6fbfff")
         .attr("clip-path", "url(#clip)")
         .on("mouseover", function() { price.style("display", null); })
         .on("mouseout", function() { price.style("display", "none"); })
@@ -463,7 +485,11 @@ function generateTimeCharts(data,cf){
                 d = x0 - d0.key > d1.key - x0 ? d1 : d0;
             price.attr("transform", "translate(" + (x(d.key)+margin.left) + "," + (y(d.value)+margin.top) + ")");
             var value = d.value<100 ? d.value.toPrecision(3) : Math.round(d.value);
-            price.select("text").text(value);
+            var m_names = new Array('Jan', 'Feb', 'Mar', 
+                'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 
+                'Oct', 'Nov', 'Dec'); 
+            var date = m_names[d.key.getMonth()] + '-' + d.key.getFullYear();
+            price.select("text").text(date+": "+value);
         });
 
     focus.append("g")
@@ -487,15 +513,22 @@ function generateTimeCharts(data,cf){
 
     context.append("g")
         .attr("class", "x brush")
+        //.call(brush.extent(x2.domain()))
         .call(brush)
         .selectAll("rect")
-          .attr("y", -6)
-          .attr("height", height2 + 7);
+            .attr("y", -6)
+            .attr("height", height2+6)
+            .style({
+                "stroke-width":2,
+                "stroke":"#6fbfff",
+                "fill-opacity": "0"
+            });  
+
   
     main_chart.append("text")
         .attr("class", "y wfplabel ylabel")
         .attr("text-anchor", "end")
-        .attr("y", 20)
+        .attr("y", 0)
         .attr("x",-30)
         .attr("dy", ".75em")
         .attr("transform", "rotate(-90)")
@@ -504,24 +537,34 @@ function generateTimeCharts(data,cf){
     $('#main_chart').append('<a id="mainchartdownload" href="">Download Data</a>');
     $('#mainchartdownload').click(function(event){
         event.preventDefault();
-        downloadData(data,'Date');
+        downloadData(data,'Date',title);
     });
     
     var dates = brush.empty() ? x2.domain() : brush.extent();
     var dateFormatted = monthNames[dates[0].getMonth()] +" " + dates[0].getFullYear() + " - " +  monthNames[dates[1].getMonth()] +" " + dates[1].getFullYear();
     
-    $("#dateextent").html("Average Price for period " + dateFormatted);     
+    $("#dateextent").html("Average Price for period " + dateFormatted);
   
     function brushed() {
       x.domain(brush.empty() ? x2.domain() : brush.extent());
       focus.select(".area").attr("d", area);
       focus.select(".x.axis").call(xAxis);
       focus.selectAll(".priceline").attr("d", line); 
-    }      
+    }
+    
+    function setBrushExtent(data,months){
+        var domain = d3.extent(data.map(function(d) { return d.key; }));  
+        var endDate = domain[1];
+        var tempDate = new Date(endDate.getFullYear(), endDate.getMonth()-months, endDate.getDate());
+        var begDate = tempDate < domain[0] ? domain[0] : tempDate;
+        d3.select(".brush").call(brush.extent([begDate,endDate]));
+        brushed();
+    }
 }
 
-function downloadData(data,name){
+function downloadData(data,name,title){
     var csvContent = 'data:text/csv;charset=utf-8,';
+    csvContent += title+'\n\n';
     csvContent += name+',Price\n';
     var m_names = new Array('January', 'February', 'March', 
     'April', 'May', 'June', 'July', 'August', 'September', 
@@ -724,7 +767,7 @@ function getCountryIDs(){
     $.ajax({
       type: 'POST',
       dataType: 'json',
-      url: 'https://demo-data.hdx.rwlabs.org/api/3/action/datastore_search_sql',
+      url: 'https://test-data.hdx.rwlabs.org/api/3/action/datastore_search_sql',
       data: data,
       success: function(data) {
           var results = [];
@@ -744,7 +787,7 @@ function getProductDataByCountryID(adm0_code,cm_id,um_id,adm0_name,cm_name,um_na
     $.ajax({
       type: 'POST',
       dataType: 'json',
-      url: 'https://demo-data.hdx.rwlabs.org/api/3/action/datastore_search_sql',
+      url: 'https://test-data.hdx.rwlabs.org/api/3/action/datastore_search_sql',
       data: data,
       success: function(data) {
 
@@ -770,7 +813,7 @@ function getProductsByCountryID(adm0_code,adm0_name){
     $.ajax({
       type: 'POST',
       dataType: 'json',
-      url: 'https://demo-data.hdx.rwlabs.org/api/3/action/datastore_search_sql',
+      url: 'https://test-data.hdx.rwlabs.org/api/3/action/datastore_search_sql',
       data: data,
       success: function(data) {
           generateSparklines(data.result.records,adm0_code,adm0_name);
@@ -788,7 +831,7 @@ function getNameParams(adm0,prod,unit,adm1,mkt){
             $.ajax({
               type: 'POST',
               dataType: 'json',
-              url: 'https://demo-data.hdx.rwlabs.org/api/3/action/datastore_search_sql',
+              url: 'https://test-data.hdx.rwlabs.org/api/3/action/datastore_search_sql',
               data: data,
               success: function(data) {              
                   initCountry(adm0,data.result.records[0].adm0_name);
@@ -803,7 +846,7 @@ function getNameParams(adm0,prod,unit,adm1,mkt){
         $.ajax({
             type: 'POST',
             dataType: 'json',
-            url: 'https://demo-data.hdx.rwlabs.org/api/3/action/datastore_search_sql',
+            url: 'https://test-data.hdx.rwlabs.org/api/3/action/datastore_search_sql',
             data: data,
             success: function(data) {              
                 if(adm1=='Not found'){
@@ -873,6 +916,7 @@ function parseGet(val) {
 }
 
 function initembed(){
+    $('#header').height(60);
     var size = parseGet('size');
     var prod = parseGet('prod');
     var unit = parseGet('unit');
@@ -930,7 +974,7 @@ function initHDX(){
     });     
 }
 
-var datastoreID = 'eb966967-07c8-4a82-8742-b3867fe44b23';
+var datastoreID = '9e1eb333-333e-4a1f-acfe-f0b43ba73379';
 var url = 'http://127.0.0.1:8000/index.html';
 
 var embedded = (parseGet('embedded'));
